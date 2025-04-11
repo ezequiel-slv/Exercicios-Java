671f26926ccd9a52e85c2f44f4266ef1af09a22b
Ezequiel <ezequielsilvalima01@gmail.com>
2025-04-11 20:13:26 -0300

    commit

---
+++ src/arquivos/algoritimos/TipoPrimitivo.java
@@ -0,0 +1,15 @@
+package arquivos.algoritimos;
+
+public class TipoPrimitivo {
+    public static void main(String[] args) {
+        //int
+        System.out.println("------");
+        int valor1 = 200;
+        int valor2 = 300;
+
+        int resultado = valor1 + valor2;
+        System.out.println("valor1 + valor2: " + resultado);
+
+
+    }
+}
