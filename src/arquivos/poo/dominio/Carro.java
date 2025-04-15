3f8f4de3a52a4cd68531ac3e1f239bc7c7e00daa
Ezequiel <ezequielsilvalima01@gmail.com>
2025-04-15 18:02:21 -0300

    commit

---
+++ src/arquivos/poo/dominio/Carro.java
@@ -0,0 +1,31 @@
+package arquivos.poo.dominio;
+
+public class Carro {
+    public String modelo;
+    public int ano;
+    public int velocidade;
+
+    public void MostrarDados(){
+        System.out.println("--------");
+        System.out.println(this.modelo);
+        System.out.println(this.ano);
+        System.out.println(this.velocidade);
+    }
+
+    public void AumentaVelocidade(Carro altavel){
+        System.out.println("-------");
+        int velocidade = altavel.velocidade;
+        if (velocidade <= 20) {
+            System.out.println(velocidade + 70);
+        }
+    }
+
+    public void DiminuiVelocidade(Carro baixavel){
+        System.out.println("-------");
+        int velocidade = baixavel.velocidade;
+        if (velocidade >= 120){
+            System.out.println(velocidade - 30);
+        }
+
+    }
+}
---
+++ src/arquivos/poo/teste/TesteCarro.java
@@ -0,0 +1,30 @@
+package arquivos.poo.teste;
+
+import arquivos.poo.dominio.Carro;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Carro altavel = new Carro();
+        Carro baixavel = new Carro();
+
+        carro.ano = 2023;
+        carro.modelo = "volkswagen polo 2023";
+        carro.velocidade = 100;
+        carro.MostrarDados();
+
+        altavel.ano = 2023;
+        altavel.modelo = "volkswagen polo 2023";
+        altavel.velocidade = 20;
+        altavel.AumentaVelocidade(altavel);
+
+        baixavel.ano = 2023;
+        baixavel.modelo = "volkswagen polo 2023";
+        baixavel.velocidade = 120;
+        baixavel.DiminuiVelocidade(baixavel);
+
+
+
+
+    }
+}
